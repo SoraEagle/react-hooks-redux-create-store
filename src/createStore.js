7a16c855d14adacb44c6f32351574bee38092853
Matthew Phillips <phillips.matthew829@gmail.com>
2022-11-18 12:35:26 -0600

    commit tree 12142dfe08fde577b635f5d95c1a3db3794d8532

--- src/createStore.js
+++ src/createStore.js
@@ -1,28 +1,43 @@
-let state;
+function createStore(){
+  let state;
 
-function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  }
+
+  return {dispatch, getState};
+}
+
+function reducer(state = {count: 0}, action){
+  switch (action.type){
     case "counter/increment":
-      return { count: state.count + 1 };
-
+      return {count: state.count + 1};
+      case "counter/decrement":
+        return {count: state.count - 1};
     default:
       return state;
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
-function render() {
+function render(){
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+let store = createStore();
+store.dispatch({type: "@@INIT"});
 let button = document.getElementById("button");
+let minus = document.getElementById("minus");
 
-button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+button.addEventListener("click", function(){
+  store.dispatch({type: "counter/increment"});
+});
+
+minus.addEventListener("click", function(){
+  store.dispatch({type: "counter/decrement"});
 });
